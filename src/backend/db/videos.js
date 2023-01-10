1c56d131b5e20f87e6b5b710674b7235de3b4c13
Parth <parolekarparth@gmail.com>
2023-01-10 22:51:35 +0530

    new videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -31,4 +31,49 @@ export const videos = [
     thumbnail: "https://img.youtube.com/vi/gQmiqmxJMtA/maxresdefault.jpg",
     src: "https://www.youtube.com/embed/gQmiqmxJMtA",
   },
+  {
+    _id: "bMy9g0USMd8",
+    title: "2022 Most Satisfying Framework??! This Survey Surprised Me",
+    description:
+      "Jamstack survey was wild this year! Thank you Netlify for running this every year and sharing the results",
+    creator: "Theo - t3 gg",
+    thumbnail: "https://img.youtube.com/vi/bMy9g0USMd8/maxresdefault.jpg",
+    src: "https://www.youtube.com/embed/bMy9g0USMd8",
+  },
+  {
+    _id: "F_Riqjdh2oM",
+    title: "React JS Crash Course",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Traversy Media",
+    thumbnail: "https://img.youtube.com/vi/w7ejDZ8SWv8/maxresdefault.jpg",
+    src: "https://www.youtube.com/embed/w7ejDZ8SWv8",
+  },
+  {
+    _id: "F_Riqjdh2oM",
+    title: "React JS Crash Course",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Traversy Media",
+    thumbnail: "https://img.youtube.com/vi/w7ejDZ8SWv8/maxresdefault.jpg",
+    src: "https://www.youtube.com/embed/w7ejDZ8SWv8",
+  },
+  {
+    _id: "F_Riqjdh2oM",
+    title: "React JS Crash Course",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Traversy Media",
+    thumbnail: "https://img.youtube.com/vi/w7ejDZ8SWv8/maxresdefault.jpg",
+    src: "https://www.youtube.com/embed/w7ejDZ8SWv8",
+  },
+  {
+    _id: "F_Riqjdh2oM",
+    title: "React JS Crash Course",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Traversy Media",
+    thumbnail: "https://img.youtube.com/vi/w7ejDZ8SWv8/maxresdefault.jpg",
+    src: "https://www.youtube.com/embed/w7ejDZ8SWv8",
+  },
 ];
